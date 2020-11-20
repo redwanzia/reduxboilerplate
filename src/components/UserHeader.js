68a133fd08305f199e20262caf6ab400d9e17165
redwan zia <redwan.zia@gmail.com>
2020-11-19 22:56:53 -0500

    addeduserHeadder component

---
+++ src/components/UserHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class UserHeader extends Component {
+  render() {
+    return (
+      <div>
+        user header
+      </div>
+    )
+  }
+}
+
+export default UserHeader
